fc8e4442cd6e49662b1da36a717f16d82b1e89a7
Kelvin Luck <kelvin.luck@crowdstrike.com>
2020-02-04 22:04:27 -0400

    chore: alphabeticise props
    So that we know where to add a new one...

--- index.js
+++ index.js
@@ -19,21 +19,21 @@ module.exports = {
     'keyword-spacing': ['error', {}],
     'max-params': ['error', 3],
     'no-constant-condition': 'off',
+    'no-empty': ['error', { allowEmptyCatch: true }],
     'one-var': ['error', 'never'],
     'quotes': ['error', 'single', {
       'allowTemplateLiterals': true,
       'avoidEscape': true,
     }],
+    'require-await': 'error',
+    'semi': 'error',
     'spaced-comment': ['error', 'always', {
       'block': {
         'balanced': true,
         'exceptions': ['*'],
       },
     }],
-    'template-curly-spacing': ['error', 'never'],
-    'semi': 'error',
-    'no-empty': ['error', { allowEmptyCatch: true }],
-    'require-await': 'error',
     'strict': 'error',
+    'template-curly-spacing': ['error', 'never'],
   },
 };
