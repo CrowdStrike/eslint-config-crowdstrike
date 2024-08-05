59cbfe59ccabf9a4765ba999b81c970141443948
Charles Lim <charles.lim@crowdstrike.com>
2024-08-05 16:25:54 -0700

    fix: pin eslint-config-crowdstrike to published version, to ensure stable peer dependency with eslint-config-crowdstrike-node during development

--- index.js
+++ index.js
@@ -1,86 +1,87 @@
-import js from "@eslint/js";
+'use strict';
 
-export default [
+const js = require('@eslint/js');
+
+module.exports = [
   js.configs.recommended,
   {
-    ignores: ["!**/.*"],
     languageOptions: {
-        globals: {},
-        ecmaVersion: 2022,
-        sourceType: "script",
+      globals: {},
+      ecmaVersion: 2022,
+      sourceType: 'commonjs',
     },
 
     rules: {
-        "comma-dangle": ["error", "always-multiline"],
-        "eol-last": "error",
-        eqeqeq: "error",
+      'comma-dangle': ['error', 'always-multiline'],
+      'eol-last': 'error',
+      eqeqeq: 'error',
 
-        indent: ["error", 2, {
-            MemberExpression: 1,
-            SwitchCase: 1,
-        }],
+      indent: ['error', 2, {
+        MemberExpression: 1,
+        SwitchCase: 1,
+      }],
 
-        "max-params": ["error", 3],
-        "no-constant-condition": "off",
+      'max-params': ['error', 3],
+      'no-constant-condition': 'off',
 
-        "no-empty": ["error", {
-            allowEmptyCatch: true,
-        }],
+      'no-empty': ['error', {
+        allowEmptyCatch: true,
+      }],
 
-        "no-eval": "error",
-        "no-restricted-globals": ["error", "event"],
-        "object-shorthand": "error",
-        "one-var": ["error", "never"],
+      'no-eval': 'error',
+      'no-restricted-globals': ['error', 'event'],
+      'object-shorthand': 'error',
+      'one-var': ['error', 'never'],
 
-        quotes: ["error", "single", {
-            allowTemplateLiterals: true,
-            avoidEscape: true,
-        }],
+      quotes: ['error', 'single', {
+        allowTemplateLiterals: true,
+        avoidEscape: true,
+      }],
 
-        "require-await": "error",
-        semi: "error",
-        "arrow-spacing": "error",
-        "array-bracket-spacing": "error",
-        "block-spacing": "error",
-        "comma-spacing": "error",
-        "computed-property-spacing": "error",
-        "func-call-spacing": "error",
+      'require-await': 'error',
+      semi: 'error',
+      'arrow-spacing': 'error',
+      'array-bracket-spacing': 'error',
+      'block-spacing': 'error',
+      'comma-spacing': 'error',
+      'computed-property-spacing': 'error',
+      'func-call-spacing': 'error',
 
-        "generator-star-spacing": ["error", {
-            named: "after",
-            anonymous: "after",
-        }],
+      'generator-star-spacing': ['error', {
+        named: 'after',
+        anonymous: 'after',
+      }],
 
-        "key-spacing": "error",
-        "keyword-spacing": ["error", {}],
-        "no-multi-spaces": "error",
-        "no-trailing-spaces": "error",
-        "no-whitespace-before-property": "error",
-        "object-curly-spacing": ["error", "always"],
-        "rest-spread-spacing": "error",
-        "semi-spacing": "error",
-        "space-before-blocks": "error",
+      'key-spacing': 'error',
+      'keyword-spacing': ['error', {}],
+      'no-multi-spaces': 'error',
+      'no-trailing-spaces': 'error',
+      'no-whitespace-before-property': 'error',
+      'object-curly-spacing': ['error', 'always'],
+      'rest-spread-spacing': 'error',
+      'semi-spacing': 'error',
+      'space-before-blocks': 'error',
 
-        "space-before-function-paren": ["error", {
-            anonymous: "ignore",
-            named: "never",
-            asyncArrow: "always",
-        }],
+      'space-before-function-paren': ['error', {
+        anonymous: 'ignore',
+        named: 'never',
+        asyncArrow: 'always',
+      }],
 
-        "space-in-parens": "error",
-        "space-infix-ops": "error",
-        "space-unary-ops": "error",
+      'space-in-parens': 'error',
+      'space-infix-ops': 'error',
+      'space-unary-ops': 'error',
 
-        "spaced-comment": ["error", "always", {
-            block: {
-                balanced: true,
-                exceptions: ["*"],
-            },
-        }],
+      'spaced-comment': ['error', 'always', {
+        block: {
+          balanced: true,
+          exceptions: ['*'],
+        },
+      }],
 
-        "switch-colon-spacing": "error",
-        "template-curly-spacing": ["error", "never"],
-        "template-tag-spacing": "error",
-        "yield-star-spacing": "error",
+      'switch-colon-spacing': 'error',
+      'template-curly-spacing': ['error', 'never'],
+      'template-tag-spacing': 'error',
+      'yield-star-spacing': 'error',
     },
-}];
+  }];
